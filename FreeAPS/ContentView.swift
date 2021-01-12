d6f87f3fa692f96e9c2d631481dadbe4f15c6492
Ivan Valkou <zloyfey@gmail.com>
2021-01-12 11:25:53 +0300

    Initial Commit

---
+++ FreeAPS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FreeAPS
+//
+//  Created by Ivan Valkou on 12.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FreeAPS/FreeAPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeAPSApp.swift
+//  FreeAPS
+//
+//  Created by Ivan Valkou on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct FreeAPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
