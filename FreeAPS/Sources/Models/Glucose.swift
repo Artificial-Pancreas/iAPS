1cb3dfb8a4f5a76532c28ea220e80652361b8667
Ivan Valkou <zloyfey@gmail.com>
2021-02-06 20:54:58 +0300

    Glucose model

---
+++ FreeAPS/Sources/Models/Glucose.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+struct Glucose: JSON {
+    let sgv: Int?
+    let glucose: Int?
+    let type: GlucoseType
+    let noise: Int?
+    let date: Date
+    let filtered: Double?
+    let direction: Direction?
+}
+
+enum GlucoseType: String, JSON {
+    case sgv
+    case cal
+}
+
+enum Direction: String, JSON {
+    case tripleUp = "TripleUp"
+    case doubleUp = "DoubleUp"
+    case singleUp = "SingleUp"
+    case fortyFiveUp = "FortyFiveUp"
+    case flat = "Flat"
+    case fortyFiveDown = "FortyFiveDown"
+    case singleDown = "SingleDown"
+    case doubleDown = "DoubleDown"
+    case tripleDown = "TripleDown"
+    case none = "NONE"
+    case notComputable = "NOT COMPUTABLE"
+    case rateOutOfRange = "RATE OUT OF RANGE"
+}
