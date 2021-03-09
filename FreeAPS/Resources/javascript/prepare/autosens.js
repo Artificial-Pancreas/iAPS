9de72bd50ae62403f9f83f7d3c5fdb17d8f3a42d
zubernuker <ii.ddavydov@gmail.com>
2021-03-09 21:30:25 +0300

    add news autosens function name for bundle and glucose data check
    As describet here: https://github.com/openaps/oref0/blob/master/bin/oref0-detect-sensitivity.js

--- FreeAPS/Resources/javascript/prepare/autosens.js
+++ FreeAPS/Resources/javascript/prepare/autosens.js
@@ -1,7 +1,11 @@
+// для settings/autosens.json параметры: monitor/glucose.json monitor/pumphistory-24h-zoned.json settings/basal_profile.json settings/profile.json monitor/carbhistory.json settings/temptargets.json
 var printLog = function(...args) {};
 var process = { stderr: { write: printLog } };
 
-function generate(pumphistory_data, profile_data, carb_data, glucose_data, basalprofile, temptarget_data) {
+function generate(glucose_data, pumphistory_data, basalprofile, profile_data, carb_data = {}, temptarget_data = {}) {
+    if (glucose_data.length < 72) {
+        return {"ratio": 1, "error": "not enough glucose data to calculate autosens"};
+    }
     var iob_inputs = {
         history: pumphistory_data,
         profile: profile_data
@@ -15,8 +19,9 @@ function generate(pumphistory_data, profile_data, carb_data, glucose_data, basal
         temptargets: temptarget_data
     };
     detection_inputs.deviations = 96;
-    var ratio8h = freeaps(detection_inputs);
+    var ratio8h = freeaps_autosens(detection_inputs);
     detection_inputs.deviations = 288;
-    var ratio24h = freeaps(detection_inputs);
-    return ratio8h.ratio < ratio24h.ratio ? ratio8h : ratio24h
+    var ratio24h = freeaps_autosens(detection_inputs);
+    var lowestRatio = ratio8h.ratio < ratio24h.ratio ? ratio8h : ratio24h;
+    return {"ratio": lowestRatio};
 }
