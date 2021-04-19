312ba63a1ee3e42c4b9d201451eca6ee51dde0de
Jon Mårtensson <jon.m@live.se>
2021-04-19 11:36:11 +0200

    Just one decimal in delta BG now
    (cherry picked from commit 470c43e22882104a16e7b812caad10ae83c2a5e1)

--- FreeAPS/Sources/Modules/Home/View/Header/CurrentGlucoseView.swift
+++ FreeAPS/Sources/Modules/Home/View/Header/CurrentGlucoseView.swift
@@ -19,7 +19,7 @@ struct CurrentGlucoseView: View {
     private var deltaFormatter: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
-        formatter.maximumFractionDigits = 2
+        formatter.maximumFractionDigits = 1
         formatter.positivePrefix = "+"
         return formatter
     }
