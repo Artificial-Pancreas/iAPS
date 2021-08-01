c9f5b9e7186ae7ae4697859097424d65553a50fc
Jon Mårtensson <jon.m@live.se>
2021-08-01 16:59:19 +0200

    oref0 variables infoTexts

--- FreeAPS/Sources/Modules/PreferencesEditor/View/PreferencesEditorRootView.swift
+++ FreeAPS/Sources/Modules/PreferencesEditor/View/PreferencesEditorRootView.swift
@@ -1,5 +1,11 @@
 import SwiftUI
 
+struct InfoText: Identifiable {
+    var id: String { description }
+    let description: String
+    let oref0Variable: String
+}
+
 extension PreferencesEditor {
     struct RootView: BaseView {
         @EnvironmentObject var viewModel: ViewModel<Provider>
@@ -9,6 +15,8 @@ extension PreferencesEditor {
             formatter.numberStyle = .decimal
             return formatter
         }
+
+        @State private var infoButtonPressed: InfoText?
 
         var body: some View {
             Form {
@@ -36,17 +44,24 @@ extension PreferencesEditor {
                     }
 
                     ForEach(viewModel.boolFields.indexed(), id: \.1.id) { index, field in
-                        Toggle(field.displayName, isOn: self.$viewModel.boolFields[index].value)
+                        HStack {
+                            Button("", action: {
+                                infoButtonPressed = InfoText(description: field.infoText, oref0Variable: field.displayName)
+                            })
+                            Toggle(field.displayName, isOn: self.$viewModel.boolFields[index].value)
+                        }
                     }
 
                     ForEach(viewModel.decimalFields.indexed(), id: \.1.id) { index, field in
                         HStack {
+                            Button("", action: {
+                                infoButtonPressed = InfoText(description: field.infoText, oref0Variable: field.displayName)
+                            })
                             Text(field.displayName)
                             DecimalTextField("0", value: self.$viewModel.decimalFields[index].value, formatter: formatter)
                         }
                     }
                 }
-
                 Section {
                     Text("Edit settings json").chevronCell()
                         .navigationLink(to: .configEditor(file: OpenAPS.FreeAPS.settings), from: self)
@@ -54,6 +69,13 @@ extension PreferencesEditor {
             }
             .navigationTitle("Preferences")
             .navigationBarTitleDisplayMode(.automatic)
+            .alert(item: $infoButtonPressed) { infoButton in
+                Alert(
+                    title: Text("\(infoButton.oref0Variable)"),
+                    message: Text("\(infoButton.description)"),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
         }
     }
 }
