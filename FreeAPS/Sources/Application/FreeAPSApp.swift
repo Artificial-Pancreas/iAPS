25e8dcac667ee7ee2460899705c9f16a9172863d
Ivan Valkou <zloyfey@gmail.com>
2021-11-07 12:06:37 +0300

    Fix objects scope

--- FreeAPS/Sources/Application/FreeAPSApp.swift
+++ FreeAPS/Sources/Application/FreeAPSApp.swift
@@ -2,59 +2,59 @@ import SwiftUI
 import Swinject
 
 @main struct FreeAPSApp: App {
-	@Environment(\.scenePhase) var scenePhase
+    @Environment(\.scenePhase) var scenePhase
 
-	// Dependencies Assembler
-	// contain all dependencies Assemblies
-	// TODO: Remove static key after update "Use Dependencies" logic
-	private static var assembler = Assembler([
-		StorageAssembly(),
-		ServiceAssembly(),
-		APSAssembly(),
-		NetworkAssembly(),
-		UIAssembly(),
-		SecurityAssembly()
-	])
+    // Dependencies Assembler
+    // contain all dependencies Assemblies
+    // TODO: Remove static key after update "Use Dependencies" logic
+    private static var assembler = Assembler([
+        StorageAssembly(),
+        ServiceAssembly(),
+        APSAssembly(),
+        NetworkAssembly(),
+        UIAssembly(),
+        SecurityAssembly()
+    ], parent: nil, defaultObjectScope: .container)
 
-	var resolver: Resolver {
-		FreeAPSApp.assembler.resolver
-	}
+    var resolver: Resolver {
+        FreeAPSApp.assembler.resolver
+    }
 
-	// Temp static var
-	// Use to backward compatibility with old Dependencies logic on Logger
-	// TODO: Remove var after update "Use Dependencies" logic in Logger
-	static var resolver: Resolver {
-		FreeAPSApp.assembler.resolver
-	}
+    // Temp static var
+    // Use to backward compatibility with old Dependencies logic on Logger
+    // TODO: Remove var after update "Use Dependencies" logic in Logger
+    static var resolver: Resolver {
+        FreeAPSApp.assembler.resolver
+    }
 
-	private func loadServices() {
-		resolver.resolve(AppearanceManager.self)!.setupGlobalAppearance()
-		_ = resolver.resolve(DeviceDataManager.self)!
-		_ = resolver.resolve(APSManager.self)!
-		_ = resolver.resolve(FetchGlucoseManager.self)!
-		_ = resolver.resolve(FetchTreatmentsManager.self)!
-		_ = resolver.resolve(FetchAnnouncementsManager.self)!
-	}
+    private func loadServices() {
+        resolver.resolve(AppearanceManager.self)!.setupGlobalAppearance()
+        _ = resolver.resolve(DeviceDataManager.self)!
+        _ = resolver.resolve(APSManager.self)!
+        _ = resolver.resolve(FetchGlucoseManager.self)!
+        _ = resolver.resolve(FetchTreatmentsManager.self)!
+        _ = resolver.resolve(FetchAnnouncementsManager.self)!
+    }
 
-	init() {
-		loadServices()
-	}
+    init() {
+        loadServices()
+    }
 
-	var body: some Scene {
-		WindowGroup {
-			Main.Builder(resolver: resolver).buildView()
-		}
-		.onChange(of: scenePhase) { newScenePhase in
-			switch newScenePhase {
-			case .active:
-				debug(.default, "APPLICATION is active")
-			case .inactive:
-				debug(.default, "APPLICATION is inactive")
-			case .background:
-				debug(.default, "APPLICATION is in background")
-			@unknown default:
-				debug(.default, "APPLICATION: Received an unexpected scenePhase.")
-			}
-		}
-	}
+    var body: some Scene {
+        WindowGroup {
+            Main.Builder(resolver: resolver).buildView()
+        }
+        .onChange(of: scenePhase) { newScenePhase in
+            switch newScenePhase {
+            case .active:
+                debug(.default, "APPLICATION is active")
+            case .inactive:
+                debug(.default, "APPLICATION is inactive")
+            case .background:
+                debug(.default, "APPLICATION is in background")
+            @unknown default:
+                debug(.default, "APPLICATION: Received an unexpected scenePhase.")
+            }
+        }
+    }
 }
