1bfa154046830092d8d115d62e4352acc5cfe288
Ivan Valkou <zloyfey@gmail.com>
2021-12-19 23:06:01 +0300

    Improved calibrations screen

--- FreeAPS/Sources/Modules/Calibrations/View/CalibrationsChart.swift
+++ FreeAPS/Sources/Modules/Calibrations/View/CalibrationsChart.swift
@@ -3,6 +3,13 @@ import SwiftUI
 struct CalibrationsChart: View {
     @EnvironmentObject var state: Calibrations.StateModel
 
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .short
+        return formatter
+    }
+
     private let maxValue = 400.0
 
     var body: some View {
@@ -29,12 +36,21 @@ struct CalibrationsChart: View {
                 .stroke(.blue, lineWidth: 2)
 
                 ForEach(state.calibrations, id: \.self) { value in
-                    Circle().fill(.red)
-                        .frame(width: 6, height: 6)
-                        .position(
-                            x: value.x / maxValue * geo.size.width,
-                            y: geo.size.width - (value.y / maxValue * geo.size.width)
-                        )
+                    ZStack {
+                        Circle().fill(.red)
+                            .frame(width: 6, height: 6)
+                            .position(
+                                x: value.x / maxValue * geo.size.width,
+                                y: geo.size.width - (value.y / maxValue * geo.size.width)
+                            )
+                        Text(dateFormatter.string(from: value.date))
+                            .foregroundColor(.white)
+                            .font(.system(size: 10))
+                            .position(
+                                x: value.x / maxValue * geo.size.width,
+                                y: geo.size.width - (value.y / maxValue * geo.size.width) + 10
+                            )
+                    }
                 }
             }
             .frame(height: geo.size.width)
