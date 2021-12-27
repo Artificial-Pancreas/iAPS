f6e2ff297a1721aecd863ec8484f39ff09e63765
Ivan Valkou <zloyfey@gmail.com>
2021-12-27 17:11:50 +0300

    Decimal precision for sensitive values

--- FreeAPS/Sources/Models/CarbRatios.swift
+++ FreeAPS/Sources/Models/CarbRatios.swift
@@ -15,3 +15,20 @@ enum CarbUnit: String, JSON {
     case grams
     case exchanges
 }
+
+extension CarbRatioEntry {
+    private enum CodingKeys: String, CodingKey {
+        case start
+        case offset
+        case ratio
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let start = try container.decode(String.self, forKey: .start)
+        let offset = try container.decode(Int.self, forKey: .offset)
+        let ratio = try container.decode(Double.self, forKey: .ratio).decimal ?? .zero
+
+        self = CarbRatioEntry(start: start, offset: offset, ratio: ratio)
+    }
+}
