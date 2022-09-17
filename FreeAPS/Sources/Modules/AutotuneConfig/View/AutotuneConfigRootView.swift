32c0acb269a86839dc078f7db50736ecc27cf03e
avouspierre <pn.lagarde@gmail.com>
2022-09-17 14:41:35 +0200

    Total insulin : Limit to 2 fraction digits instead 3

--- FreeAPS/Sources/Modules/AutotuneConfig/View/AutotuneConfigRootView.swift
+++ FreeAPS/Sources/Modules/AutotuneConfig/View/AutotuneConfigRootView.swift
@@ -16,7 +16,7 @@ extension AutotuneConfig {
         private var rateFormatter: NumberFormatter {
             let formatter = NumberFormatter()
             formatter.numberStyle = .decimal
-            formatter.maximumFractionDigits = 3
+            formatter.maximumFractionDigits = 2
             return formatter
         }
 
