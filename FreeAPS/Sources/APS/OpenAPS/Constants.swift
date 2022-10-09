a2a9af42170f862e77fee9495184c6b4290aabf4
Jon Mårtensson <jon.m@live.se>
2022-10-09 19:54:21 +0200

    Bad merge. Delete duplicate variable

--- FreeAPS/Sources/APS/OpenAPS/Constants.swift
+++ FreeAPS/Sources/APS/OpenAPS/Constants.swift
@@ -85,7 +85,6 @@ extension OpenAPS {
         static let uploadedCGMState = "upload/uploaded-cgm-state.json"
         static let uploadedPodAge = "upload/uploaded-pod-age.json"
         static let uploadedProfile = "upload/uploaded-profile.json"
-        static let uploadedPodAge = "upload/uploaded-pod-age.json"
     }
 
     enum FreeAPS {
