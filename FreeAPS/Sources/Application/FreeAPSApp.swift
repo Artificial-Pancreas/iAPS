05c91b8c0dd5dfe62b7d09f0a439e837b543bf0d
Jon Mårtensson <jon.m@live.se>
2022-11-11 12:12:13 +0100

    dailyStats. Logic for time interval fix. Version update.

--- FreeAPS/Sources/Application/FreeAPSApp.swift
+++ FreeAPS/Sources/Application/FreeAPSApp.swift
@@ -64,9 +64,11 @@ extension Bundle {
     var releaseVersionNumber: String? {
         infoDictionary?["CFBundleShortVersionString"] as? String
     }
+
     var buildVersionNumber: String? {
         infoDictionary?["CFBundleVersion"] as? String
     }
+
     var buildDate: Date {
         if let infoPath = Bundle.main.path(forResource: "Info", ofType: "plist"),
            let infoAttr = try? FileManager.default.attributesOfItem(atPath: infoPath),
