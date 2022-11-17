1e58091c49d85005f4da91ba1ef4c22268329074
Marc G. Fournier <scrappy@hub.org>
2022-11-17 03:13:40 -0800

    Add Helps/Bundle+Extensions.swift (#502)
    Move Bundle extension from FreeAPSApp.swift -> Helpers file to be
    consistent with other Extensions

--- FreeAPS/Sources/Application/FreeAPSApp.swift
+++ FreeAPS/Sources/Application/FreeAPSApp.swift
@@ -59,23 +59,3 @@ import Swinject
         }
     }
 }
-
-extension Bundle {
-    var releaseVersionNumber: String? {
-        infoDictionary?["CFBundleShortVersionString"] as? String
-    }
-
-    var buildVersionNumber: String? {
-        infoDictionary?["CFBundleVersion"] as? String
-    }
-
-    var buildDate: Date {
-        if let infoPath = Bundle.main.path(forResource: "Info", ofType: "plist"),
-           let infoAttr = try? FileManager.default.attributesOfItem(atPath: infoPath),
-           let infoDate = infoAttr[.modificationDate] as? Date
-        {
-            return infoDate
-        }
-        return Date()
-    }
-}
