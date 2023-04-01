ae8e64f222389e8256f8f937878188f9c4614969
Jon Mårtensson <jon.m@live.se>
2023-04-01 14:24:24 +0200

    LW xcode fix

--- FreeAPS/Sources/Modules/LibreViewConfig/View/LibreViewConfigRootView.swift
+++ FreeAPS/Sources/Modules/LibreViewConfig/View/LibreViewConfigRootView.swift
@@ -88,10 +88,10 @@ extension LibreViewConfig {
             } header: {
                 Text("Credentials")
             } footer: {
-                if state.lastUpload > 0,
-                   let lastUploadDate = Date(timeIntervalSince1970: state.lastUpload),
-                   let nextuploadDate = Date(timeIntervalSince1970: state.lastUpload + state.nextUploadDelta)
-                {
+                if state.lastUpload > 0 {
+                    let lastUploadDate = Date(timeIntervalSince1970: state.lastUpload)
+                    let nextuploadDate = Date(timeIntervalSince1970: state.lastUpload + state.nextUploadDelta)
+
                     Text(
                         "Last upload on \(state.dateFormatter.string(from: lastUploadDate)). Next upload no earlier than \(state.dateFormatter.string(from: nextuploadDate))"
                     )
