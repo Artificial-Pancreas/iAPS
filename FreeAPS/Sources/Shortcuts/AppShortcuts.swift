0de3ee724bcb25de6ea905fa155e316f766062bd
Pierre L <pn.lagarde@gmail.com>
2023-05-06 13:58:23 +0200

    First implementation of siri shortcuts

---
+++ FreeAPS/Sources/Shortcuts/AppShortcuts.swift
@@ -0,0 +1,21 @@
+import AppIntents
+import Foundation
+
+@available(iOS 16.0, *) struct AppShortcuts: AppShortcutsProvider {
+    @AppShortcutsBuilder static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: ApplyTempPresetIntent(),
+            phrases: [
+                "Activate \(.applicationName) temporary target ?",
+                "\(.applicationName) apply a temporary target"
+            ]
+        )
+        AppShortcut(
+            intent: ListStateIntent(),
+            phrases: [
+                "List \(.applicationName) state",
+                "\(.applicationName) state"
+            ]
+        )
+    }
+}
---
+++ FreeAPS/Sources/Shortcuts/TempPresets/ListTempPresetsIntent.swift
@@ -0,0 +1,43 @@
+import AppIntents
+import Foundation
+
+@available(iOS 16.0, *) struct ListTempPresetsIntent: AppIntent {
+    // Title of the action in the Shortcuts app
+    static var title: LocalizedStringResource = "Choose Temporary Presets"
+
+    // Description of the action in the Shortcuts app
+    static var description = IntentDescription(
+        "Allow to list and choose a specific temporary Preset.",
+        categoryName: "Navigation"
+    )
+
+    @Parameter(title: "Preset") var preset: tempPreset?
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("Choose the temp preset  \(\.$preset)")
+    }
+
+    @MainActor func perform() async throws -> some ReturnsValue<tempPreset> {
+        .result(
+            value: preset!
+        )
+    }
+}
+
+@available(iOS 16.0, *) struct tempPresetsQuery: EntityQuery {
+    internal var intentRequest: TempPresetsIntentRequest
+
+    init() {
+        intentRequest = TempPresetsIntentRequest()
+    }
+
+    func entities(for identifiers: [tempPreset.ID]) async throws -> [tempPreset] {
+        let tempTargets = intentRequest.fetchIDs(identifiers)
+        return tempTargets
+    }
+
+    func suggestedEntities() async throws -> [tempPreset] {
+        let tempTargets = intentRequest.fetchAll()
+        return tempTargets
+    }
+}
