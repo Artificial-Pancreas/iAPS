7e0a35b2cb7fea04cafec07b6359031c3ae5e01a
Jon Mårtensson <jon.m@live.se>
2023-05-31 12:50:03 +0200

    Clean up

--- FreeAPS/Sources/Modules/TargetsEditor/View/TargetsEditorRootView.swift
+++ FreeAPS/Sources/Modules/TargetsEditor/View/TargetsEditorRootView.swift
@@ -69,20 +69,6 @@ extension TargetsEditor {
                         }
                         .frame(maxWidth: geometry.size.width / 3)
                         .clipped()
-
-                        /*
-                         Picker(selection: $state.items[index].highIndex, label: EmptyView()) {
-                             ForEach(0 ..< state.rateValues.count, id: \.self) { i in
-                                 Text(
-                                     self.rateFormatter
-                                         .string(from: state.rateValues[i] as NSNumber) ?? ""
-                                 ).tag(i)
-                             }
-                         }
-                         .frame(maxWidth: geometry.size.width / 3)
-                         .clipped()
-                          */
-
                         Picker(selection: $state.items[index].timeIndex, label: EmptyView()) {
                             ForEach(0 ..< state.timeValues.count, id: \.self) { i in
                                 Text(
@@ -109,12 +95,6 @@ extension TargetsEditor {
                             Text(
                                 "\(rateFormatter.string(from: state.rateValues[item.lowIndex] as NSNumber) ?? "0")"
                             )
-                            /*
-                             Text("–").foregroundColor(.secondary)
-                             Text(
-                                 "\(rateFormatter.string(from: state.rateValues[item.highIndex] as NSNumber) ?? "0")"
-                             )
-                             */
                             Text("\(state.units.rawValue)").foregroundColor(.secondary)
                             Spacer()
                             Text("starts at").foregroundColor(.secondary)
