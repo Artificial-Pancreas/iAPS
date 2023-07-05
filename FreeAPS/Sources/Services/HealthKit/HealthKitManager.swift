4c31ac8f8c2658c4539b47a8d933483ca88c31db
MikePlante1 <82073483+MikePlante1@users.noreply.github.com>
2023-07-05 02:30:18 -0400

    Update HealthKitManager.swift

--- FreeAPS/Sources/Services/HealthKit/HealthKitManager.swift
+++ FreeAPS/Sources/Services/HealthKit/HealthKitManager.swift
@@ -327,9 +327,9 @@ final class BaseHealthKitManager: HealthKitManager, Injectable, CarbsObserver {
 
         let query = HKObserverQuery(sampleType: bgType, predicate: nil) { [weak self] _, _, observerError in
             guard let self = self else { return }
-            debug(.service, "Execute HelathKit observer query for loading increment samples")
+            debug(.service, "Execute HealthKit observer query for loading increment samples")
             guard observerError == nil else {
-                warning(.service, "Error during execution of HelathKit Observer's query", error: observerError!)
+                warning(.service, "Error during execution of HealthKit Observer's query", error: observerError!)
                 return
             }
 
