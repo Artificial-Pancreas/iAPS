cc158e2660e2b307bb527a2e3766801f6a8432c7
Pierre L <pn.lagarde@gmail.com>
2023-10-15 15:23:47 +0200

    Fix healthkit issue for Insulin treatment
    Fix the healthKit sync when stopped before the end #231
    Fix the 0U basal in healthKit #149

--- FreeAPS/Sources/APS/APSManager.swift
+++ FreeAPS/Sources/APS/APSManager.swift
@@ -72,7 +72,6 @@ final class BaseAPSManager: APSManager, Injectable {
     @Injected() private var nightscout: NightscoutManager!
     @Injected() private var settingsManager: SettingsManager!
     @Injected() private var broadcaster: Broadcaster!
-    @Injected() private var healthKitManager: HealthKitManager!
     @Persisted(key: "lastAutotuneDate") private var lastAutotuneDate = Date()
     @Persisted(key: "lastStartLoopDate") private var lastStartLoopDate: Date = .distantPast
     @Persisted(key: "lastLoopDate") var lastLoopDate: Date = .distantPast {
@@ -267,10 +266,6 @@ final class BaseAPSManager: APSManager, Injectable {
     // Loop exit point
     private func loopCompleted(error: Error? = nil, loopStatRecord: LoopStats) {
         isLooping.send(false)
-
-        // save AH events
-        let events = pumpHistoryStorage.recent()
-        healthKitManager.saveIfNeeded(pumpEvents: events)
 
         if let error = error {
             warning(.apsManager, "Loop failed with error: \(error.localizedDescription)")
