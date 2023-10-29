23f4e9b03110392e37d2a91b1374825a25f66b90
Jon Mårtensson <jon.m@live.se>
2023-10-29 02:29:45 +0100

    Always display warning when over maxBolus

--- FreeAPS/Sources/Modules/DataTable/View/DataTableRootView.swift
+++ FreeAPS/Sources/Modules/DataTable/View/DataTableRootView.swift
@@ -299,8 +299,7 @@ extension DataTable {
                             DatePicker("Date", selection: $state.nonPumpInsulinDate, in: ...Date())
                         }
 
-                        let amountWarningCondition = (state.nonPumpInsulinAmount > state.maxBolus) &&
-                            (state.nonPumpInsulinAmount <= state.maxBolus * 3)
+                        let amountWarningCondition = (state.nonPumpInsulinAmount > state.maxBolus)
 
                         Section {
                             HStack {
@@ -315,8 +314,7 @@ extension DataTable {
                                 .foregroundColor(amountWarningCondition ? Color.white : Color.accentColor)
                                 .frame(maxWidth: .infinity, alignment: .center)
                                 .disabled(
-                                    state.nonPumpInsulinAmount <= 0 || state.nonPumpInsulinAmount > state
-                                        .maxBolus * 3
+                                    state.nonPumpInsulinAmount <= 0 || state.nonPumpInsulinAmount > state.maxBolus * 3
                                 )
                             }
                         }
