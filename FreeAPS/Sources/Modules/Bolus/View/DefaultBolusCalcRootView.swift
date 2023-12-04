93f43cfa48ec1ac21cfc532e453b47c78f5e9076
Jon Mårtensson <jon.m@live.se>
2023-12-04 22:17:58 +0100

    Hide also in deafult calc

--- FreeAPS/Sources/Modules/Bolus/View/DefaultBolusCalcRootView.swift
+++ FreeAPS/Sources/Modules/Bolus/View/DefaultBolusCalcRootView.swift
@@ -16,6 +16,7 @@ extension Bolus {
         @State private var keepForNextWiew: Bool = false
 
         @Environment(\.colorScheme) var colorScheme
+        @FocusState private var isFocused: Bool
 
         @FetchRequest(
             entity: Meals.entity(),
@@ -81,19 +82,28 @@ extension Bolus {
                                 }
                         }.contentShape(Rectangle())
                     }
-
                     HStack {
                         Text("Amount")
+                        if isFocused {
+                            Button { isFocused = false } label: {
+                                HStack {
+                                    Text("Hide").foregroundStyle(.gray)
+                                    Image(systemName: "keyboard")
+                                        .symbolRenderingMode(.monochrome).foregroundStyle(colorScheme == .dark ? .white : .black)
+                                }.padding(.leading)
+                            }
+                            .controlSize(.mini)
+                        }
                         Spacer()
                         DecimalTextField(
                             "0",
                             value: $state.amount,
                             formatter: formatter,
-                            autofocus: true,
                             cleanInput: true
                         )
                         Text(!(state.amount > state.maxBolus) ? "U" : "😵").foregroundColor(.secondary)
                     }
+                    .focused($isFocused)
 
                 } header: { Text("Bolus") }
 
