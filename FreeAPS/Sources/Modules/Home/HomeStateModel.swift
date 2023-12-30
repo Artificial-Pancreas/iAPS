c3f6bea56d5899da086d482e4155cc38120c29c3
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2023-12-30 19:57:12 +0100

    Updates
    * Bolus progress. Display units currently delivered in the bolus progress view
    
    * Highlight TT when activated.Disable by tap.Long press to see TT or Profile Override settings.
    
    * Make Loop border thicker
    
    * Clean up PumpView. Display when no pod is active. Reduce HStack size (length) for the PumpView.
    
    * Increase Header text contast in dark mode
    * Simplify Cancel TT and Profile from Home View
    * Color code highlighted buttons
    
    * Bring back Time Buttons
    * Localizations
    * Clean up

--- FreeAPS/Sources/Modules/Home/HomeStateModel.swift
+++ FreeAPS/Sources/Modules/Home/HomeStateModel.swift
@@ -9,6 +9,7 @@ extension Home {
         @Injected() var broadcaster: Broadcaster!
         @Injected() var apsManager: APSManager!
         @Injected() var nightscoutManager: NightscoutManager!
+        @Injected() var storage: TempTargetsStorage!
         private let timer = DispatchTimer(timeInterval: 5)
         private(set) var filteredHours = 24
         @Published var glucose: [BloodGlucose] = []
@@ -43,6 +44,7 @@ extension Home {
         @Published var errorMessage: String? = nil
         @Published var errorDate: Date? = nil
         @Published var bolusProgress: Decimal?
+        @Published var bolusAmount: Decimal?
         @Published var eventualBG: Int?
         @Published var carbsRequired: Decimal?
         @Published var allowManualTemp = false
@@ -171,6 +173,11 @@ extension Home {
                 .weakAssign(to: \.bolusProgress, on: self)
                 .store(in: &lifetime)
 
+            apsManager.bolusAmount
+                .receive(on: DispatchQueue.main)
+                .weakAssign(to: \.bolusAmount, on: self)
+                .store(in: &lifetime)
+
             apsManager.pumpDisplayState
                 .receive(on: DispatchQueue.main)
                 .sink { [weak self] state in
@@ -224,6 +231,21 @@ extension Home {
         func cancelProfile() {
             OverrideStorage().cancelProfile()
             setupOverrideHistory()
+        }
+
+        func cancelTempTarget() {
+            storage.storeTempTargets([TempTarget.cancel(at: Date())])
+            coredataContext.performAndWait {
+                let saveToCoreData = TempTargets(context: self.coredataContext)
+                saveToCoreData.active = false
+                saveToCoreData.date = Date()
+                try? self.coredataContext.save()
+
+                let setHBT = TempTargetsSlider(context: self.coredataContext)
+                setHBT.enabled = false
+                setHBT.date = Date()
+                try? self.coredataContext.save()
+            }
         }
 
         private func setupGlucose() {
