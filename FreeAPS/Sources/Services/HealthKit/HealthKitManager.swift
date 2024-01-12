8a839ef89991569f5c94f04b2dbdaf8d01cf0ef3
Deniz Cengiz <48965855+dnzxy@users.noreply.github.com>
2024-01-13 00:43:17 +0100

    Add more verbose logging

--- FreeAPS/Sources/Services/HealthKit/HealthKitManager.swift
+++ FreeAPS/Sources/Services/HealthKit/HealthKitManager.swift
@@ -171,10 +171,15 @@ final class BaseHealthKitManager: HealthKitManager, Injectable, CarbsObserver, P
 
             healthKitStore.save(samplesToSave) { (success: Bool, error: Error?) -> Void in
                 if success {
-                    debug(.service, "Saved blood glucose")
+                    for sample in samplesToSave {
+                        debug(
+                            .service,
+                            "Stored blood glucose \(sample.quantity) in HealthKit Store! Metadata: \(String(describing: sample.metadata?.values))"
+                        )
+                    }
                 } else {
-                    debug(.service, "Failed saving blood glucose")
-                    debug(.service, error!.localizedDescription)
+                    debug(.service, "Failed to store blood glucose in HealthKit Store!")
+                    debug(.service, error?.localizedDescription ?? "Unknown error")
                 }
             }
         }
@@ -202,7 +207,7 @@ final class BaseHealthKitManager: HealthKitManager, Injectable, CarbsObserver, P
             let sampleDates = samples.map(\.startDate)
             let samplesToSave = carbsWithId
                 .filter { !sampleIDs.contains($0.id ?? "") } // id existing in AH
-                .filter { !sampleDates.contains($0.actualDate ?? $0.createdAt) } // not id but exaclty the same datetime
+                .filter { !sampleDates.contains($0.actualDate ?? $0.createdAt) } // not id but exactly the same datetime
                 .map {
                     HKQuantitySample(
                         type: sampleType,
@@ -220,10 +225,15 @@ final class BaseHealthKitManager: HealthKitManager, Injectable, CarbsObserver, P
 
             healthKitStore.save(samplesToSave) { (success: Bool, error: Error?) -> Void in
                 if success {
-                    debug(.service, "Saved carb entry")
+                    for sample in samplesToSave {
+                        debug(
+                            .service,
+                            "Stored carb entry \(sample.quantity) in HealthKit Store! Metadata: \(String(describing: sample.metadata?.values))"
+                        )
+                    }
                 } else {
-                    debug(.service, "Failed saving carb entry")
-                    debug(.service, error!.localizedDescription)
+                    debug(.service, "Failed to store carb entry in HealthKit Store!")
+                    debug(.service, error?.localizedDescription ?? "Unknown error")
                 }
             }
         }
@@ -292,10 +302,15 @@ final class BaseHealthKitManager: HealthKitManager, Injectable, CarbsObserver, P
 
             healthKitStore.save(bolusSamples + basalSamples) { (success: Bool, error: Error?) -> Void in
                 if success {
-                    debug(.service, "Saved insulin delivery")
+                    for sample in bolusSamples + basalSamples {
+                        debug(
+                            .service,
+                            "Stored insulin entry in HealthKit Store! Metadata: \(String(describing: sample.metadata?.values))"
+                        )
+                    }
                 } else {
-                    debug(.service, "Failed saving insulin delivery")
-                    debug(.service, error!.localizedDescription)
+                    debug(.service, "Failed to store insulin entry in HealthKit Store!")
+                    debug(.service, error?.localizedDescription ?? "Unknown error")
                 }
             }
         }
