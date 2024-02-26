3030a35a85426f9b2f75c4c6fc6068ff83d1dbe7
Jon Mårtensson <jon.m@live.se>
2024-02-26 17:51:17 +0100

    Refactor. Remove duplicate code in APSManager. Clean up code in OpenAPSManager. Don't delete those overides that run to completion in Nigtscout. Bump version.

--- FreeAPS/Sources/APS/Storage/CoreDataStorage.swift
+++ FreeAPS/Sources/APS/Storage/CoreDataStorage.swift
@@ -19,4 +19,75 @@ final class CoreDataStorage {
         }
         return fetchGlucose
     }
+
+    func fetchTDD(interval: NSDate) -> [TDD] {
+        var uniqueEvents = [TDD]()
+        coredataContext.performAndWait {
+            let requestTDD = TDD.fetchRequest() as NSFetchRequest<TDD>
+            requestTDD.predicate = NSPredicate(format: "timestamp > %@ AND tdd > 0", interval)
+            let sortTDD = NSSortDescriptor(key: "timestamp", ascending: true)
+            requestTDD.sortDescriptors = [sortTDD]
+            try? uniqueEvents = coredataContext.fetch(requestTDD)
+        }
+        return uniqueEvents
+    }
+
+    func fetchTempTargetsSlider() -> [TempTargetsSlider] {
+        var sliderArray = [TempTargetsSlider]()
+        coredataContext.performAndWait {
+            let requestIsEnbled = TempTargetsSlider.fetchRequest() as NSFetchRequest<TempTargetsSlider>
+            let sortIsEnabled = NSSortDescriptor(key: "date", ascending: false)
+            requestIsEnbled.sortDescriptors = [sortIsEnabled]
+            // requestIsEnbled.fetchLimit = 1
+            try? sliderArray = coredataContext.fetch(requestIsEnbled)
+        }
+        return sliderArray
+    }
+
+    func fetchTempTargets() -> [TempTargets] {
+        var tempTargetsArray = [TempTargets]()
+        coredataContext.performAndWait {
+            let requestTempTargets = TempTargets.fetchRequest() as NSFetchRequest<TempTargets>
+            let sortTT = NSSortDescriptor(key: "date", ascending: false)
+            requestTempTargets.sortDescriptors = [sortTT]
+            requestTempTargets.fetchLimit = 1
+            try? tempTargetsArray = coredataContext.fetch(requestTempTargets)
+        }
+        return tempTargetsArray
+    }
+
+    func fetcarbs(interval: NSDate) -> [Carbohydrates] {
+        var carbs = [Carbohydrates]()
+        coredataContext.performAndWait {
+            let requestCarbs = Carbohydrates.fetchRequest() as NSFetchRequest<Carbohydrates>
+            requestCarbs.predicate = NSPredicate(format: "carbs > 0 AND date > %@", interval)
+            let sortCarbs = NSSortDescriptor(key: "date", ascending: true)
+            requestCarbs.sortDescriptors = [sortCarbs]
+            try? carbs = coredataContext.fetch(requestCarbs)
+        }
+        return carbs
+    }
+
+    func fetchStats() -> [StatsData] {
+        var stats = [StatsData]()
+        coredataContext.performAndWait {
+            let requestStats = StatsData.fetchRequest() as NSFetchRequest<StatsData>
+            let sortStats = NSSortDescriptor(key: "lastrun", ascending: false)
+            requestStats.sortDescriptors = [sortStats]
+            requestStats.fetchLimit = 1
+            try? stats = coredataContext.fetch(requestStats)
+        }
+        return stats
+    }
+
+    func fetchInsulinDistribution() -> [InsulinDistribution] {
+        var insulinDistribution = [InsulinDistribution]()
+        coredataContext.performAndWait {
+            let requestInsulinDistribution = InsulinDistribution.fetchRequest() as NSFetchRequest<InsulinDistribution>
+            let sortInsulin = NSSortDescriptor(key: "date", ascending: false)
+            requestInsulinDistribution.sortDescriptors = [sortInsulin]
+            try? insulinDistribution = coredataContext.fetch(requestInsulinDistribution)
+        }
+        return insulinDistribution
+    }
 }
