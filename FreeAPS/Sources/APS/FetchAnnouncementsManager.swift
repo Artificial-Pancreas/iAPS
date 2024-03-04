1fa24730dec36879faaae76a410cbe4a19bfb0cb
Jon Mårtensson <jon.m@live.se>
2024-03-04 12:33:36 +0100

    Decrese timeInterval to Allow 2 attempts of fetching latest announcement.

--- FreeAPS/Sources/APS/FetchAnnouncementsManager.swift
+++ FreeAPS/Sources/APS/FetchAnnouncementsManager.swift
@@ -13,7 +13,7 @@ final class BaseFetchAnnouncementsManager: FetchAnnouncementsManager, Injectable
     @Injected() var settingsManager: SettingsManager!
 
     private var lifetime = Lifetime()
-    private let timer = DispatchTimer(timeInterval: 5.minutes.timeInterval)
+    private let timer = DispatchTimer(timeInterval: 4.minutes.timeInterval)
 
     init(resolver: Resolver) {
         injectServices(resolver)
