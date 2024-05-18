e9c3458b5b8c50106a6138ec289ba261483fa85c
Jon Mårtensson <jon.m@live.se>
2024-05-19 01:15:37 +0200

    Always log and display in pop-up when dynISF disabled by exercise
    (cherry picked from commit dbb480aca6236fcb29d0dd42c1a91e3bb7999a2c)

--- FreeAPS/Resources/javascript/prepare/middleware.js
+++ FreeAPS/Resources/javascript/prepare/middleware.js
@@ -13,6 +13,10 @@ function generate(iob, currenttemp, glucose, profile, autosens = null, meal = nu
         if (profile.tddAdjBasal && dynamicVariables.average_total_data != 0) {
             profile.tdd_factor = Math.round((dynamicVariables.weightedAverage / dynamicVariables.average_total_data) * 100) / 100;
         }
+        if (profile.useNewFormula && profile.temptargetSet && (profile.high_temptarget_raises_sensitivity || profile.exercise_mode || dynamicVariables.isEnabled) && profile.min_bg >= 118) {
+                profile.useNewFormula = false;
+                console.log("Dynamic ISF disabled due to an active exercise ");
+        }
         
         return profile;
     } catch (error) {
