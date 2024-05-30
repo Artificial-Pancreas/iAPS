dc733d980c2db943fabdd82598fb3ba540ec8e4b
Marc G. Fournier <scrappy@hub.org>
2024-05-30 03:57:07 -0700

    Fix Typo: birtDate -> birthDate (#722)
    Set default birthDate to distantPast ( current default is "today", making everyone an infant )

--- FreeAPS/Sources/APS/APSManager.swift
+++ FreeAPS/Sources/APS/APSManager.swift
@@ -1207,7 +1207,7 @@ final class BaseAPSManager: APSManager, Injectable {
                     Variance: variance
                 ),
                 id: getIdentifier(),
-                dob: settingsManager.settings.birtDate,
+                dob: settingsManager.settings.birthDate,
                 sex: settingsManager.settings.sexSetting
             )
             storage.save(dailystat, as: file)
