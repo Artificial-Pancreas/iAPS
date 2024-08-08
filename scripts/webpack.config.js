8f9f42059280513a6f6175934948558f89a1a6b1
Thomas Vargiu <tvargiu@gmail.com>
2024-08-08 03:49:04 +0200

    use original oref0 profile

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,21 +1,23 @@
 const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
 
+const libPath = process.env['OREF0_DIST_PATH'] || './oref0/lib'
+
 module.exports = {
   mode: 'production',
   entry: {
-    iob: './lib/iob/index.js',
-    meal: './lib/meal/index.js',
-    "determineBasal": './lib/determine-basal/determine-basal.js',
-    "glucoseGetLast": './lib/glucose-get-last.js',
-    "basalSetTemp": './lib/basal-set-temp.js',
-    autosens: './lib/determine-basal/autosens.js',
-    profile: './lib/profile/index.js',
-    "autotunePrep": './lib/autotune-prep/index.js',
-    "autotuneCore": './lib/autotune/index.js'
+    iob: path.resolve(libPath, 'iob/index.js'),
+    meal: path.resolve(libPath, 'meal/index.js'),
+    determineBasal: path.resolve(libPath, 'determine-basal/determine-basal.js'),
+    glucoseGetLast: path.resolve(libPath, 'glucose-get-last.js'),
+    basalSetTemp: path.resolve(libPath, 'basal-set-temp.js'),
+    autosens: path.resolve(libPath, 'determine-basal/autosens.js'),
+    profile: path.resolve(libPath, 'profile/index.js'),
+    autotunePrep: path.resolve(libPath, 'autotune-prep/index.js'),
+    autotuneCore: path.resolve(libPath, 'autotune/index.js')
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '..', 'FreeAPS', 'Resources', 'javascript', 'bundle'),
     filename: (pathData) => {
         return pathData.chunk.name.replace(/[A-Z]/g, function(match) {
             return '-' + match.toLowerCase();
@@ -25,7 +27,15 @@ module.exports = {
     library: 'freeaps_[name]'
   },
   optimization: {
+    sideEffects: true,
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+            format: {
+                comments: false,
+            },
+        },
+    })],
   },
 };
