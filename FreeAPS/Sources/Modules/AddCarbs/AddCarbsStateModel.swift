87fd146d9072ace985d422985383ab31814dc81c
Jon Mårtensson <jon.m@live.se>
2024-09-12 16:12:12 +0200

    Display Bolus suggestion when not using hypo treatment

--- FreeAPS/Sources/Modules/AddCarbs/AddCarbsStateModel.swift
+++ FreeAPS/Sources/Modules/AddCarbs/AddCarbsStateModel.swift
@@ -62,7 +62,7 @@ extension AddCarbs {
 
             if hypoTreatment { hypo() }
 
-            if let profile = id, profile != "None" || (skipBolus && !continue_ && !fetch) {
+            if hypoTreatment, let profile = id, profile != "None" || (skipBolus && !continue_ && !fetch) {
                 apsManager.determineBasalSync()
                 showModal(for: nil)
             } else if carbs > 0 {
