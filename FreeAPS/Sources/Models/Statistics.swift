4c2df8b2b9431c901a7de16ca28a4a17109795ad
Jon B.M <jon.m@live.se>
2024-10-14 10:00:52 +0200

    Upload most frequent loop error
    Resolve minimum duration

--- FreeAPS/Sources/Models/Statistics.swift
+++ FreeAPS/Sources/Models/Statistics.swift
@@ -103,6 +103,7 @@ extension Statistics {
 struct LoopCycles: JSON, Equatable {
     var loops: Int
     var errors: Int
+    var mostFrequentErrorType: String
     var readings: Int
     var success_rate: Decimal
     var avg_interval: Decimal
@@ -172,6 +173,7 @@ extension LoopCycles {
     private enum CodingKeys: String, CodingKey {
         case loops
         case errors
+        case mostFrequentErrorType
         case readings
         case success_rate
         case avg_interval
