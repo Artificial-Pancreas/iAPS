a30dd721314b979cc2b8fb076a0928f2e9ab449d
Iurii Malchenko <i@yurique.com>
2025-01-17 00:30:04 +0100

    DecimalTextField - fix compilation

--- FreeAPS/Sources/Views/DecimalTextField.swift
+++ FreeAPS/Sources/Views/DecimalTextField.swift
@@ -180,7 +180,7 @@ extension DecimalTextField.Coordinator: UITextFieldDelegate {
         if let text = textField.text {
             let newText = (text as NSString).replacingCharacters(in: range, with: string)
 
-            let decimalSeparatorCount = newText.count(where: { $0 == parent.formatter.decimalSeparator.first })
+            let decimalSeparatorCount = newText.count(where: { $0 == (parent.formatter.decimalSeparator.first ?? ".") })
             if decimalSeparatorCount > 1 {
                 return false
             }
