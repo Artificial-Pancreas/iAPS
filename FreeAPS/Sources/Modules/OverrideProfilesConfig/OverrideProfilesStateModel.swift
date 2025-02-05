2f5412816d3921c1bd95643378ab31f4be1cfc00
Jon B.M <jon.m@live.se>
2025-02-05 20:23:54 +0100

    Sometimes we need a new object

--- FreeAPS/Sources/Modules/OverrideProfilesConfig/OverrideProfilesStateModel.swift
+++ FreeAPS/Sources/Modules/OverrideProfilesConfig/OverrideProfilesStateModel.swift
@@ -351,7 +351,11 @@ extension OverrideProfilesConfig {
 
         // Save Auto ISF Override settings
         func updateAutoISF(_ id_: String?) {
-            guard let identifier = id_, let saveAutoISF = OverrideStorage().fetchAutoISFsetting(id: identifier) else { return }
+            guard let identifier = id_ else { return }
+            
+            let oldObject = OverrideStorage().fetchAutoISFsetting(id: identifier)
+            let saveAutoISF = oldObject != nil ? oldObject! : Auto_ISF(context: coredataContext)
+            
             coredataContext.perform { [self] in
                 saveAutoISF.autoISFhourlyChange = autoISFsettings.autoISFhourlyChange as NSDecimalNumber
                 saveAutoISF.autoisf = autoISFsettings.autoisf
@@ -381,6 +385,7 @@ extension OverrideProfilesConfig {
                 saveAutoISF.smbDeliveryRatioBGrange = autoISFsettings.smbDeliveryRatioBGrange as NSDecimalNumber
                 saveAutoISF.postMealISFweight = autoISFsettings.postMealISFweight as NSDecimalNumber
                 saveAutoISF.date = Date.now
+                if oldObject == nil { saveAutoISF.id = identifier }
                 try? self.coredataContext.save()
             }
         }
