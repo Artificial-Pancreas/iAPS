9c1b686ae6def76c87b07614eb7a7608c153901f
Jon Mårtensson <jon.m@live.se>
2025-02-05 14:05:28 +0100

    Remove borders

--- FreeAPS/Sources/Modules/OverrideProfilesConfig/View/OverrideProfilesRootView.swift
+++ FreeAPS/Sources/Modules/OverrideProfilesConfig/View/OverrideProfilesRootView.swift
@@ -644,7 +644,7 @@ extension OverrideProfilesConfig {
 
             if name != "" {
                 VStack(alignment: .leading) {
-                    Text(name).padding(.top, 5)
+                    Text(name).padding(.top, 5).padding(.bottom, 2)
                     HStack(spacing: 7) {
                         percent != 1 ?
                             Text(percent.formatted(.percent.grouping(.never).rounded().precision(.fractionLength(0))))
@@ -659,7 +659,6 @@ extension OverrideProfilesConfig {
                         }
                         Spacer()
                     }
-                    .padding(.top, 2)
                     .font(.caption)
 
                     if preset.advancedSettings {
@@ -693,7 +692,9 @@ extension OverrideProfilesConfig {
                                 decimal(decimal: aisf.autoisf_min, setting: standard.autoisf_min, label: "Min: ")
                                 decimal(decimal: aisf.autoisf_max, setting: standard.autoisf_max, label: "Max: ")
                             }
-                        }.foregroundStyle(.secondary).font(.caption)
+                        }
+                        .offset(y: 2)
+                        .foregroundStyle(.secondary).font(.caption)
 
                         HStack(spacing: 5) {
                             percentage(
@@ -787,7 +788,7 @@ extension OverrideProfilesConfig {
         /// Round to two fraction digits
         private func bool(bool: Bool, setting: Bool, label: String) -> AnyView? {
             if bool != setting {
-                return Text(label + "\(bool)").foregroundStyle(.white).boolTag(bool).asAny()
+                return Text(label + (bool ? "on" : "off")).foregroundStyle(.white).boolTag(bool).asAny()
             }
             return nil
         }
