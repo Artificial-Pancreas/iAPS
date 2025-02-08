a3bb249ecbb0e25cc30de3f8e6f02de5a770933a
Jon B.M <jon.m@live.se>
2025-02-08 17:41:58 +0100

    Consistent Spacing
    Use auto horisontal spacing and fixed vertical spacing in preset View to make preset list item layout consistent. Adjust for the bigger BoolTags.
    
    Format using  hours and minutes for the override duration.
    
    Avoid text wrapping
    
    And some other small updates.

--- FreeAPS/Sources/Views/ViewModifiers.swift
+++ FreeAPS/Sources/Views/ViewModifiers.swift
@@ -20,33 +20,14 @@ struct RoundedBackground: ViewModifier {
     }
 }
 
-struct CapsulaBackground: ViewModifier {
-    private let color: Color
-
-    init(color: Color = Color("CapsuleColor")) {
-        self.color = color
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .padding()
-            .background(
-                Rectangle()
-                    // Capsule()
-                    .fill()
-                    .foregroundColor(color)
-            )
-    }
-}
-
 struct BoolTag: ViewModifier {
-    @Environment(\.colorScheme) var colorScheme
     let bool: Bool
+    @Environment(\.colorScheme) var colorScheme
     func body(content: Content) -> some View {
         content
             .padding(.vertical, 4).padding(.horizontal, 6)
             .background((bool ? Color.green : Color.red).opacity(colorScheme == .light ? 0.8 : 0.5))
-            .clipShape(RoundedRectangle(cornerRadius: 6)).padding(.trailing, 6)
+            .clipShape(RoundedRectangle(cornerRadius: 6)).padding(.vertical, 3).padding(.trailing, 3)
     }
 }
 
@@ -149,7 +130,6 @@ struct ColouredRoundedBackground: View {
     @Environment(\.colorScheme) var colorScheme
     var body: some View {
         Rectangle()
-            // RoundedRectangle(cornerRadius: 15)
             .fill(
                 colorScheme == .dark ? .black :
                     Color.white
@@ -305,36 +285,6 @@ struct Link<T>: ViewModifier where T: View {
     }
 }
 
-struct AdaptsToSoftwareKeyboard: ViewModifier {
-    @State var currentHeight: CGFloat = 0
-
-    func body(content: Content) -> some View {
-        content
-            .padding(.bottom, currentHeight).animation(.easeOut(duration: 0.25))
-            .edgesIgnoringSafeArea(currentHeight == 0 ? Edge.Set() : .bottom)
-            .onAppear(perform: subscribeToKeyboardChanges)
-    }
-
-    private let keyboardHeightOnOpening = Foundation.NotificationCenter.default
-        .publisher(for: UIResponder.keyboardWillShowNotification)
-        .map { $0.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect }
-        .map(\.height)
-
-    private let keyboardHeightOnHiding = Foundation.NotificationCenter.default
-        .publisher(for: UIResponder.keyboardWillHideNotification)
-        .map { _ in CGFloat(0) }
-
-    private func subscribeToKeyboardChanges() {
-        _ = Publishers.Merge(keyboardHeightOnOpening, keyboardHeightOnHiding)
-            .subscribe(on: DispatchQueue.main)
-            .sink { height in
-                if self.currentHeight == 0 || height == 0 {
-                    self.currentHeight = height
-                }
-            }
-    }
-}
-
 struct ClearButton: ViewModifier {
     @Binding var text: String
     func body(content: Content) -> some View {
@@ -390,10 +340,6 @@ extension View {
 
     func navigationLink<V: BaseView>(to screen: Screen, from view: V) -> some View {
         modifier(Link(destination: view.state.view(for: screen), screen: screen))
-    }
-
-    func adaptsToSoftwareKeyboard() -> some View {
-        modifier(AdaptsToSoftwareKeyboard())
     }
 
     func modal<V: BaseView>(for screen: Screen?, from view: V) -> some View {
