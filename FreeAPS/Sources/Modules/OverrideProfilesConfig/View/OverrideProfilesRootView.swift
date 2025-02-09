77424e5dfb25c594f7b0a3598fe6372c6b51ec29
Jon B.M <jon.m@live.se>
2025-02-09 16:31:21 +0100

    Truncate, when necessary
    as some Crowdin translations are longer.
    A preparation for the Crowdin merge.

--- FreeAPS/Sources/Modules/OverrideProfilesConfig/View/OverrideProfilesRootView.swift
+++ FreeAPS/Sources/Modules/OverrideProfilesConfig/View/OverrideProfilesRootView.swift
@@ -689,20 +689,14 @@ extension OverrideProfilesConfig {
                     // All of the Auto ISF Settings (Bool and Decimal optionals)
                     if preset.overrideAutoISF, let aisf = autoisfSettings, aisf.autoisf {
                         let standard = state.currentSettings
-
                         HStack {
-                            bool(
-                                bool: aisf.enableBGacceleration,
-                                setting: standard.enableBGacceleration,
-                                label: "Accel"
-                            )
-                            bool(bool: aisf.ketoProtect, setting: standard.ketoProtect, label: "Keto")
-                            bool(bool: aisf.use_B30, setting: standard.use_B30, label: "B30")
+                            bool(bool: aisf.enableBGacceleration, setting: standard.enableBGacceleration, label: "Accel")
+                                .frame(maxHeight: 30)
+                            bool(bool: aisf.ketoProtect, setting: standard.ketoProtect, label: "Keto").frame(maxHeight: 30)
+                            bool(bool: aisf.use_B30, setting: standard.use_B30, label: "B30").frame(maxHeight: 30)
 
-                            HStack {
-                                decimal(decimal: aisf.autoisf_min, setting: standard.autoisf_min, label: "Min: ")
-                                decimal(decimal: aisf.autoisf_max, setting: standard.autoisf_max, label: "Max: ")
-                            }
+                            decimal(decimal: aisf.autoisf_min, setting: standard.autoisf_min, label: "Min: ")
+                            decimal(decimal: aisf.autoisf_max, setting: standard.autoisf_max, label: "Max: ")
                         }
                         .foregroundStyle(.secondary).font(.caption)
 
@@ -772,7 +766,7 @@ extension OverrideProfilesConfig {
         }
 
         private var edit: some View {
-            overridesView
+            overridesView.dynamicTypeSize(...DynamicTypeSize.xxLarge)
         }
 
         private func unChanged() -> Bool {
