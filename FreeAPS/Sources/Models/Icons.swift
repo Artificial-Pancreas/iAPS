7a53f36b4cf5fd6683fd14053dae0609c9e2e541
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2025-04-20 23:53:01 +0200

    Release 6.4.7
    RESOLVED ISSUES:
    * Auto ISF typo.
    * Update CGM expiration constant for L2 sensors (SAGE UI).
    * Always respect the target bg override in auto isf, by @yurique.
    * Use Xcode 16 version when building with GitHub. Xcode 16.
    * Icon Chooser Fix. Icon Chooser now working with all Xcode versions
    * Image and custom colour naming updates. Use unique names.
    
    LOCALIZATION
    * Crowdin translations and new iAPS localizations. Thank you transaltors: Nicole van Elst, Hung Nguyen and luborj.
    Icon updates

--- FreeAPS/Sources/Models/Icons.swift
+++ FreeAPS/Sources/Models/Icons.swift
@@ -3,29 +3,56 @@ import Foundation
 import UIKit
 
 enum Icon_: String, CaseIterable, Identifiable {
-    case primary = "pod_colorful"
-    case artficialPancreasPurple
-    case text = "iAPS"
-    case podBlack = "podTemplateBlack"
-    case popWhite = "podTemplateWhite"
-    case cyan = "iAPS_Loop_Cyan_Text"
-    case podPurple
-    case catWithPod
-    case catWithPodWhite = "catWithPodWhiteBG"
-    case loopWhiteText = "iAPS_Loop_White_Text"
-    case loopText = "iAPS_Loop_Text"
-    case black = "iAPS_Black_Black"
-    case clean = "iAPS_Clean"
-    case purple = "iAPS_Purple"
-    case glow = "iAPS_Glow_BG"
-    case gray = "iAPS_Gray"
-    case whiteAndGray = "iAPS_WhiteAndGray"
-    case grayAndLoopNoButtons = "iAPS_NoButtons_Gray_White_BG"
-    case purpleBG = "iAPS_Purple_BG"
-    case whiteBG = "iAPS_White_BG"
-    case loop = "iAPS_Loop"
-    case pancreasVial
+    case primary = "appIcon1"
+    case appIcon2
+    case appIcon3
+    case appIcon4
+    case appIcon5
+    case appIcon6
+    case appIcon7
+    case appIcon8
+    case appIcon9
+    case appIcon10
+    case appIcon11
+    case appIcon12
+    case appIcon13
+    case appIcon14
+    case appIcon15
+    case appIcon16
+    case appIcon17
+    case appIcon18
+    case appIcon19
+    case appIcon20
+    case appIcon21
+    case appIcon22
     var id: String { rawValue }
+
+    var preview: String {
+        switch self {
+        case .primary: "Image 1"
+        case .appIcon2: "Image 2"
+        case .appIcon3: "Image 3"
+        case .appIcon4: "Image 4"
+        case .appIcon5: "Image 5"
+        case .appIcon6: "Image 6"
+        case .appIcon7: "Image 7"
+        case .appIcon8: "Image 8"
+        case .appIcon9: "Image 9"
+        case .appIcon10: "Image 10"
+        case .appIcon11: "Image 11"
+        case .appIcon12: "Image 12"
+        case .appIcon13: "Image 13"
+        case .appIcon14: "Image 14"
+        case .appIcon15: "Image 15"
+        case .appIcon16: "Image 16"
+        case .appIcon17: "Image 17"
+        case .appIcon18: "Image 18"
+        case .appIcon19: "Image 19"
+        case .appIcon20: "Image 20"
+        case .appIcon21: "Image 21"
+        case .appIcon22: "Image 22"
+        }
+    }
 }
 
 class Icons: ObservableObject, Equatable {
