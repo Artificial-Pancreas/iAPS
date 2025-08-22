49540843ab144d1bfe844a28d9cca10fc3da67a1
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2025-08-22 23:23:50 +0200

    Typos

--- Dependencies/LibreTransmitter/Sources/LibreTransmitter/Bluetooth/LibreTransmitterMetadata.swift
+++ Dependencies/LibreTransmitter/Sources/LibreTransmitter/Bluetooth/LibreTransmitterMetadata.swift
@@ -85,7 +85,7 @@ public enum SensorType: String, CustomStringConvertible {
     case libreUS14dayE6 = "E6"
     case libreProH = "70"
     case libre2Plus = "C6"
-    case libre2PlusNew = "F7"
+    case libre2PlusNew = "7F"
 
     public var description: String {
         switch self {
@@ -109,7 +109,7 @@ public extension SensorType {
 
         let start = patchInfo[0..<2].uppercased()
 
-        let choices: [String: SensorType] = ["DF": .libre1, "A2": .libre1A2, "9D": .libre2, "C5": .libre2C5, "C6": .libre2Plus, "F7": .libre2PlusNew, "E5": .libreUS14day, "E6": .libreUS14dayE6, "70": .libreProH]
+        let choices: [String: SensorType] = ["DF": .libre1, "A2": .libre1A2, "9D": .libre2, "C5": .libre2C5, "C6": .libre2Plus, "7F": .libre2PlusNew, "E5": .libreUS14day, "E6": .libreUS14dayE6, "70": .libreProH]
 
         if let res = choices[start] {
             self = res
