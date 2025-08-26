2180153217bdbe3da2f0f4f5149218ec329ab6dd
Jon B.M <jon.m@live.se>
2025-08-26 13:20:46 +0200

    Resolve a shortcut issue
    Cancel temp target wasn't listed among app shortcuts. Resolve some yellow Swift 6 warnings.
    
    Needs testing

--- FreeAPS/Sources/Shortcuts/TempPresets/tempPresetIntent.swift
+++ FreeAPS/Sources/Shortcuts/TempPresets/tempPresetIntent.swift
@@ -4,28 +4,27 @@ import Intents
 import Swinject
 
 struct tempPreset: AppEntity, Identifiable {
-    static var defaultQuery = tempPresetsQuery()
+    static let defaultQuery = tempPresetsQuery()
 
-    var id: UUID
-    var name: String
-    var targetTop: Decimal?
-    var targetBottom: Decimal?
-    var duration: Decimal
+    let id: UUID
+    let name: String
+    let targetTop: Decimal?
+    let targetBottom: Decimal?
+    let duration: Decimal
 
     var displayRepresentation: DisplayRepresentation {
         DisplayRepresentation(title: "\(name)")
     }
 
-    static var typeDisplayRepresentation: TypeDisplayRepresentation = "Presets"
+    static let typeDisplayRepresentation: TypeDisplayRepresentation = "Presets"
 
     static func convert(_ tempTarget: TempTarget) -> tempPreset {
-        var tp = tempPreset(
+        tempPreset(
             id: UUID(uuidString: tempTarget.id)!,
             name: tempTarget.displayName,
+            targetTop: tempTarget.targetTop,
+            targetBottom: tempTarget.targetBottom,
             duration: tempTarget.duration
         )
-        tp.targetTop = tempTarget.targetTop
-        tp.targetBottom = tempTarget.targetBottom
-        return tp
     }
 }
