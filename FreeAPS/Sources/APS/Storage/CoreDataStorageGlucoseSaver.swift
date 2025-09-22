cf95db54633f825a171cc03b471ef74c794c97a1
Iurii Malchenko <i@yurique.com>
2025-09-22 23:45:22 +0200

    save glucose to coredata using a dedicated background context (to avoid race conditions)

--- FreeAPS/Sources/APS/Storage/CoreDataStorageGlucoseSaver.swift
+++ FreeAPS/Sources/APS/Storage/CoreDataStorageGlucoseSaver.swift
@@ -1,11 +1,18 @@
+import CoreData
 import Foundation
 import Swinject
 
 final class CoreDataStorageGlucoseSaver: NewGlucoseObserver {
+    private let backgroundContext: NSManagedObjectContext
     private let broadcaster: Broadcaster!
 
     init(resolver: Resolver) {
         broadcaster = resolver.resolve(Broadcaster.self)!
+
+        backgroundContext = CoreDataStack.shared.persistentContainer.newBackgroundContext()
+        backgroundContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        backgroundContext.undoManager = nil
+
         subscribe()
     }
 
@@ -14,6 +21,33 @@ final class CoreDataStorageGlucoseSaver: NewGlucoseObserver {
     }
 
     func newGlucoseStored(_ bloodGlucose: [BloodGlucose]) {
-        CoreDataStorage().saveGlucoseInBackground(bloodGlucose: bloodGlucose)
+        backgroundContext.perform {
+            guard let earliestDate = bloodGlucose.min(by: { $0.dateString < $1.dateString }).map(\.dateString) else { return }
+            do {
+                let requestReadings = Readings.fetchRequest()
+                requestReadings.predicate = NSPredicate(
+                    format: "date >= %@", earliestDate.addingTimeInterval(-5.minutes) as NSDate
+                )
+
+                let existing = try self.backgroundContext.fetch(requestReadings)
+                let existingDates = Set(existing.compactMap { $0.date?.roundedTo1Second })
+
+                for bg in bloodGlucose {
+                    guard let glucose = bg.glucose,
+                          !existingDates.contains(bg.dateString.roundedTo1Second)
+                    else {
+                        continue
+                    }
+                    let dataForForStats = Readings(context: self.backgroundContext)
+                    dataForForStats.date = bg.dateString
+                    dataForForStats.glucose = Int16(glucose)
+                    dataForForStats.id = bg.id
+                    dataForForStats.direction = bg.direction?.symbol ?? "↔︎"
+                }
+                try self.backgroundContext.save()
+            } catch {
+                debug(.service, "failed to save glucose to core data: \(error)")
+            }
+        }
     }
 }
