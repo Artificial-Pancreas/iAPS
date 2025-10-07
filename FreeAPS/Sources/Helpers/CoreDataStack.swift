1ceaa6d3bfa1f7c7e97eea128a0417adf6c89bbe
Jon B.M <jon.m@live.se>
2025-10-07 18:38:41 +0200

    Batch deletion
    for testing etc.

--- FreeAPS/Sources/Helpers/CoreDataStack.swift
+++ FreeAPS/Sources/Helpers/CoreDataStack.swift
@@ -3,23 +3,23 @@ import Foundation
 
 class CoreDataStack: ObservableObject {
     init() {}
-
+    
     static let shared = CoreDataStack()
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Core_Data")
-
+        
         container.loadPersistentStores(completionHandler: { _, error in
             guard let error = error as NSError? else { return }
             debug(.apsManager, "Unresolved error: \(error), \(error.userInfo)")
         })
-
+        
         return container
     }()
-
+    
     func saveContext() {
         let context = persistentContainer.viewContext
-
+        
         context.perform {
             if context.hasChanges {
                 do {
@@ -31,8 +31,24 @@ class CoreDataStack: ObservableObject {
             }
         }
     }
-
+    
     func delete(obj: NSManagedObject) {
         persistentContainer.viewContext.delete(obj)
+    }
+    
+    func deleteBatch(entity: String) {
+        let context = persistentContainer.viewContext
+        
+        context.performAndWait {
+            let fetchRequest: NSFetchRequest<NSFetchRequestResult>
+            fetchRequest = NSFetchRequest(entityName: entity)
+            let deleteRequest = NSBatchDeleteRequest(
+                fetchRequest: fetchRequest
+            )
+            do {
+                debug(.apsManager, "Clear the \(entity) entries from CoreData.")
+                try context.execute(deleteRequest)
+            } catch { debug(.apsManager, "Couldn't delete the \(entity) entries from CoreData.") }
+        }
     }
 }
