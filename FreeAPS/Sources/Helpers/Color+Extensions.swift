6e2f018bdd143246d9329f6551390ac805a4707b
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2025-11-19 11:26:21 +0100

    Workspace settings
    * Use Xcode recommended Watch workspace settings.
    * Resolve warnings regarding custom colours.
    * Will probably need testing with older Xcode versions.
    * Resolve a couple of Swift 6 warnings.

--- FreeAPS/Sources/Helpers/Color+Extensions.swift
+++ FreeAPS/Sources/Helpers/Color+Extensions.swift
@@ -8,12 +8,8 @@ extension Color {
 
     static let glucose = Color("glucose")
 
-    static let insulin = Color("Insulin")
-
     // The loopAccent color is intended to be use as the app accent color.
     public static let loopAccent = Color("accent")
-
-    public static let warning = Color("warning")
 }
 
 // Color version of the UIColor context colors
@@ -48,30 +44,11 @@ public extension Color {
 }
 
 extension Color {
-    static let loopGray = Color("LoopGray")
-    static let loopGreen = Color("LoopGreen")
-    static let loopYellow = Color("LoopYellow")
-    static let loopRed = Color("LoopRed")
     static let loopManualTemp = Color("ManualTempBasal")
     //   static let insulin = Color("Insulin")
     static let uam = Color("UAM")
     static let zt = Color("ZT")
-    static let tempBasal = Color("TempBasal")
-    static let basal = Color("Basal")
-    static let darkerBlue = Color("DarkerBlue")
-    static let lightBlue = Color("LightBlue")
-    static let loopPink = Color("LoopPink")
-    static let lemon = Color("Lemon")
-    static let minus = Color("minus")
-    static let darkerGray = Color("darkerGray")
-    static let darkRed = Color("DarkRed")
-    static let darkGreen = Color("DarkGreen")
     static let blueComplicationBackground = Color(red: 0.1176470588, green: 0.2352941176, blue: 0.3725490196)
-    static let header = Color("Header")
-    static let header2 = Color("Header2")
     static let homeBackground = Color("HomeBackground")
-    static let popUpGray = Color("PopUpGray")
     static let darkChartBackground = Color("DarkChartBackground")
-    static let violet = Color("Violet")
-    static let purpleOverrides = Color("PurpleOverrides")
 }
