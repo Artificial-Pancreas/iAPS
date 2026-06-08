bef324fea96b4d86eda56ca488821effe3224493
Iurii Malchenko <i@yurique.com>
2026-06-08 19:42:17 +0200

    home root - start the timer on appear

--- FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
+++ FreeAPS/Sources/Modules/Home/View/HomeRootView.swift
@@ -1182,6 +1182,7 @@ extension Home {
                 }
             }
             .onAppear {
+                state.startTimer()
                 if onboarded.first?.firstRun ?? true {
                     state.fetchPreferences()
                 }
