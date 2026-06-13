3c65a368e4443e675acee68cfe6677515b94978c
Jon B.M <jon.m@live.se>
2026-06-13 11:41:32 +0200

    Display note

--- FreeAPS/Sources/Modules/AddCarbs/View/AddCarbsRootView.swift
+++ FreeAPS/Sources/Modules/AddCarbs/View/AddCarbsRootView.swift
@@ -320,7 +320,7 @@ extension AddCarbs {
 
             switch (hasFiber, microCount > 0) {
             case (false, false):
-                return "Optional"
+                return state.note
 
             case (true, false):
                 return "Fiber"
@@ -666,7 +666,7 @@ extension AddCarbs {
         ) -> String {
             switch (hasFiber, microCount > 0) {
             case (false, false):
-                return "Optional"
+                return state.note
 
             case (true, false):
                 return "Fiber"
