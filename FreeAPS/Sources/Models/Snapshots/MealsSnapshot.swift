78cc767623fc0d4724e6855490fe5943fface84c
Iurii Malchenko <i@yurique.com>
2026-07-23 22:25:52 +0200

    MealsSnapshot.isEmpty - check fiber as well

--- FreeAPS/Sources/Models/Snapshots/MealsSnapshot.swift
+++ FreeAPS/Sources/Models/Snapshots/MealsSnapshot.swift
@@ -44,6 +44,7 @@ extension MealsSnapshot {
         (carbs ?? 0) <= 0 &&
             (fat ?? 0) <= 0 &&
             (protein ?? 0) <= 0 &&
+            (fiber ?? 0) <= 0 &&
             micronutrient.allSatisfy { ($0.amount ?? 0) <= 0 }
     }
 
