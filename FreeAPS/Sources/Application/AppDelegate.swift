0057062093b3c933797d322a59dab3a5ffa3bb38
yurique <i@yurique.com>
2026-08-16 10:36:22 +0200

    [hotfix] fix services initialization regression [main] (#2020)

--- FreeAPS/Sources/Application/AppDelegate.swift
+++ FreeAPS/Sources/Application/AppDelegate.swift
@@ -2,8 +2,18 @@ import SwiftUI
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
+    /** create the object graph from here rather than from a SwiftUI view */
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        AppLauncher.shared.startIfNeeded()
+        return true
+    }
+
     func applicationProtectedDataDidBecomeAvailable(_: UIApplication) {
         debug(.default, "Protected data did become available")
+        AppLauncher.shared.startIfNeeded()
     }
 
     func applicationProtectedDataWillBecomeUnavailable(_: UIApplication) {
