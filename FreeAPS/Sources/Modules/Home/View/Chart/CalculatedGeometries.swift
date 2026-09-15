99e16744a4417ced24669b3420a98ee3f835e465
Jon B Mårtensson <jon.m@live.se>
2026-09-15 18:36:23 +0200

    Fix weird type conversion

--- FreeAPS/Sources/Modules/Home/View/Chart/CalculatedGeometries.swift
+++ FreeAPS/Sources/Modules/Home/View/Chart/CalculatedGeometries.swift
@@ -838,8 +838,9 @@ private final class GeometriesBuilder {
             var old = Array(rects)
             let duration = Double(truncating: last.duration ?? 0)
             // Looks better when target isn't == 0 in Home View Main Chart
-            let targetRaw = last.target ?? 0
-            let target = Int(truncating: targetRaw) < 6 ? 6 : targetRaw
+            let target = Int((last.target ?? 0).doubleValue.rounded())
+
+            print("Target: \(target) mg/dl")
 
             if duration > 0 {
                 let x1 = timeToXCoordinate((latest?.date ?? Date.now).timeIntervalSince1970)
@@ -848,7 +849,7 @@ private final class GeometriesBuilder {
                 let x2 = timeToXCoordinate(plusNow.timeIntervalSince1970)
                 let oneMore = CGRect(
                     x: x1,
-                    y: glucoseToYCoordinate(Int(truncating: target)) - 3,
+                    y: glucoseToYCoordinate(target) - 3,
                     width: x2 - x1,
                     height: 6
                 )
@@ -856,9 +857,7 @@ private final class GeometriesBuilder {
 
                 /// If a consecutive override is scheduled, append this lastly
                 if let preset = data.consecutiveOverride {
-                    let presetTargetRaw = preset.target ?? 0
-                    let presetTarget = Int(truncating: presetTargetRaw) < 6 ? 6 : presetTargetRaw
-
+                    let presetTarget = max(Int((preset.target ?? 0).doubleValue.rounded()), 6)
                     var presetDuration = Int(truncating: preset.duration ?? 0)
 
                     /// When set to indefinite(0) use a 48 hour illustration
@@ -875,7 +874,7 @@ private final class GeometriesBuilder {
 
                     let consecutive = CGRect(
                         x: x1_Preset,
-                        y: glucoseToYCoordinate(Int(truncating: presetTarget)) - 3,
+                        y: glucoseToYCoordinate(presetTarget) - 3,
                         width: x2_Preset - x1_Preset,
                         height: 6
                     )
@@ -891,7 +890,7 @@ private final class GeometriesBuilder {
                 let x2 = timeToXCoordinate(Date.now.timeIntervalSince1970)
                 let oneMore = CGRect(
                     x: x1,
-                    y: glucoseToYCoordinate(Int(truncating: target)) - 3,
+                    y: glucoseToYCoordinate(target) - 3,
                     width: x2 - x1 + additionalWidth,
                     height: 6
                 )
